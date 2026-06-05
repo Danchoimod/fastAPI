e180355b4ae80ed17b3189f555049381a22712a8
PhuPham <tranphupham2909@gmail.com>
2026-06-05 08:07:10 +0700

    added cor

--- src/main.py
+++ src/main.py
@@ -36,7 +36,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
-        "http://127.0.0.1:3001"
+        "http://127.0.0.1:3001",
+        "https://aurabook-kappa.vercel.app/"
     ],
 
     allow_credentials=True,
